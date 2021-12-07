19fdf22f687cb5c9c7cecbe4a67eab93afe6bafb
SAURABH-TRIPATHI17 <saurabhtrip96@gmail.com>
2021-12-07 13:29:23 +0530

    valid Parenthesis

--- Stacks and Queues/valid Parenthesis.java
+++ Stacks and Queues/valid Parenthesis.java
@@ -1,3 +1,19 @@
+/*
+Date: 07/12/2021
+Author: Saurabh Tripathi
+Problem pattern : Stacks and Queues
+
+Problem Statement:
+Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+sample 1:
+Input: s = "()[]{}"
+Output: true
+
+sample 2:
+Input: s = "([)]"
+Output: false
+*/
 class Solution {
     public boolean isValid(String s) {
         
